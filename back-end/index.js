8b7659f881166f5112a75e3239be25c3669446ec
QAU3 <cc.mijangos@gmail.com>
2023-01-14 21:37:18 +0100

    server port changed

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // SETTINGS
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5990;
 require("dotenv").config();
 const connectionParams = {
     useNewUrlParser: true,
